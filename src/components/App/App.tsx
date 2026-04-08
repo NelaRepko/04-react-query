c2f106385d216d776ed03885012b9753a58b419c
NelaRepko <repkonela@gmail.com>
2026-04-08 21:51:35 +0300

    add placeholderData and type

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -78,17 +78,17 @@ export default function App() {
 
       {/* Пагінація */}
       {totalPages > 1 && (
-        <ReactPaginate
-          pageCount={totalPages}
-          pageRangeDisplayed={5}
-          marginPagesDisplayed={1}
-          onPageChange={({ selected }) => setPage(selected + 1)}
-          forcePage={page - 1}
-          containerClassName={css.pagination}
-          activeClassName={css.active}
-          nextLabel="→"
-          previousLabel="←"
-        />
+     <ReactPaginate
+  pageCount={totalPages}
+  pageRangeDisplayed={5}
+  marginPagesDisplayed={1}
+  onPageChange={(event: { selected: number }) => setPage(event.selected + 1)}
+  forcePage={page - 1}
+  containerClassName={css.pagination}
+  activeClassName={css.active}
+  nextLabel="→"
+  previousLabel="←"
+/>
       )}
 
       {/* Модалка */}
